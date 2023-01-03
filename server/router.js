74ae19001e7d6e846fc32758b39c63d74d0a399b
Evan OShea <evanoshea21@gmail.com>
2023-01-02 16:30:33 -0800

    test get2

--- server/router.js
+++ server/router.js
@@ -1,11 +1,11 @@
 const router = require('express').Router();
 const controllers = require('../controllers')
 
+router.get('/test', controllers.test);
 
 router.get('/qa/questions', controllers.getQFromID);
 router.get('/qa/questions/:question_id/answers', controllers.getAFromID);
 router.post('/qa/questions', controllers.post);
-router.post('/test', controllers.test);
 
 
 
